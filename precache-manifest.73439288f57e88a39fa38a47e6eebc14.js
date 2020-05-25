d64a9c6639c7b6a51f807ad23d609a0d84c76dda
07aki <akshitverma@Akshits-MacBook-Pro.local>
2020-05-26 01:36:14 +0530

    Updates

---
+++ precache-manifest.73439288f57e88a39fa38a47e6eebc14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc12d7ddd70cd7199e428925a35c9fb",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "7bc5aa09814655302309",
+    "url": "/recipe-app/static/css/main.2eb14ec6.chunk.css"
+  },
+  {
+    "revision": "c51216aa07dd59323a3e",
+    "url": "/recipe-app/static/js/2.e877ef98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.e877ef98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bc5aa09814655302309",
+    "url": "/recipe-app/static/js/main.b1f97d2a.chunk.js"
+  },
+  {
+    "revision": "7ff5b27d3f9efd2f4352",
+    "url": "/recipe-app/static/js/runtime-main.96283c75.js"
+  }
+]);
